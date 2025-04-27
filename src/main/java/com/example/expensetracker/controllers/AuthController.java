8d6b7758f21f93a2bc36563c4a1e81f9b1902926
aliishov <alishov7394@gmail.com>
2025-04-27 18:55:00 +0400

    Feat: Add Service, Repository and Controller Layers

---
+++ src/main/java/com/example/expensetracker/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.expensetracker.controllers;
+
+import com.example.expensetracker.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+}
---
+++ src/main/java/com/example/expensetracker/services/UserService.java
@@ -0,0 +1,12 @@
+package com.example.expensetracker.services;
+
+import com.example.expensetracker.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
