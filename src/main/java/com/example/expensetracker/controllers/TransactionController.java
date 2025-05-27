d5811ad3ed22c870695f58ce5ca220c45de7f60a
aliishov <alishov7394@gmail.com>
2025-05-27 12:46:08 +0400

    Feat: Create Transaction Service

--- src/main/java/com/example/expensetracker/controllers/TransactionController.java
+++ src/main/java/com/example/expensetracker/controllers/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.expensetracker.controllers;
 
+import com.example.expensetracker.services.transaction.TransactionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -13,4 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Transaction Controller",
         description = "Manage Transactions in Project")
 public class TransactionController {
+
+    private final TransactionService transactionService;
 }
