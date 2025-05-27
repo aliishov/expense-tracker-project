39b9e6c276a5334480e9349ad9c5b64da3ec1992
aliishov <alishov7394@gmail.com>
2025-05-27 12:43:26 +0400

    Feat: Create Transaction Controller

---
+++ src/main/java/com/example/expensetracker/controllers/TransactionController.java
@@ -0,0 +1,16 @@
+package com.example.expensetracker.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+@Validated
+@Tag(name = "Transaction Controller",
+        description = "Manage Transactions in Project")
+public class TransactionController {
+}
