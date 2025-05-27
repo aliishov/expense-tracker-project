7ae11aa03ecbc404a7bf71a5bb7280dca45ed610
aliishov <alishov7394@gmail.com>
2025-05-27 13:03:08 +0400

    Feat: Create Transaction Converter Service

--- src/main/java/com/example/expensetracker/services/transaction/TransactionService.java
+++ src/main/java/com/example/expensetracker/services/transaction/TransactionService.java
@@ -1,11 +1,16 @@
 package com.example.expensetracker.services.transaction;
 
+import com.example.expensetracker.dtos.trasnactionDtos.TransactionRequestDto;
+import com.example.expensetracker.dtos.trasnactionDtos.TransactionResponseDto;
+import com.example.expensetracker.models.transaction.Transaction;
 import com.example.expensetracker.repositories.TransactionRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +18,13 @@ import org.springframework.stereotype.Service;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final TransactionConverter transactionConverter;
 
     private final static Marker MY_LOG_MARKER = MarkerFactory.getMarker("MY_LOGGER");
     private final static Logger LOGGER = LoggerFactory.getLogger("MY_LOGGER");
+
+    public ResponseEntity<TransactionResponseDto> create(@Valid TransactionRequestDto transactionRequestDto) {
+        LOGGER.info(MY_LOG_MARKER, "Creating new transaction");
+
+    }
 }
