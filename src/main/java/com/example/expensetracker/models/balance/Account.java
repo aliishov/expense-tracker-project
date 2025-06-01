d70f655a14c0a93ae5b934531d4b88b8e57f43c5
aliishov <alishov7394@gmail.com>
2025-06-01 12:42:00 +0400

    Feat: Create Account Entity

---
+++ src/main/java/com/example/expensetracker/models/balance/Account.java
@@ -0,0 +1,52 @@
+package com.example.expensetracker.models.balance;
+
+import com.example.expensetracker.models.enums.Currency;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+@EntityListeners(AuditingEntityListener.class)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
