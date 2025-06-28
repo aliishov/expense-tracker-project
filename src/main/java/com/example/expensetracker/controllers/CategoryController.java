55914def41c28f76713f835a9516b98d99354863
aliishov <alishov7394@gmail.com>
2025-06-28 13:56:57 +0400

    Refactor: Making Categories Controller Endpoints Rest Friendly

--- src/main/java/com/example/expensetracker/controllers/CategoryController.java
+++ src/main/java/com/example/expensetracker/controllers/CategoryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/transactions/categories")
+@RequestMapping("/categories")
 @RequiredArgsConstructor
 @Validated
 @Tag(name = "Categories Controller",
